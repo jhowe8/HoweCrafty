519c4a9118ec9d4b0ca67108425ee1b7efe5864b
jhowe8 <jshowe8@gmail.com>
2022-02-07 08:49:11 -0500

    update backend with prisma

--- frontend/src/components/WreathForm.js
+++ frontend/src/components/WreathForm.js
@@ -2,83 +2,142 @@ import './WreathForm.css'
 import React, { useEffect, useState } from "react"
 import Select from 'react-select'
 import makeAnimated from 'react-select/animated'
+import { colourOptions, seasonOptions, holidayOptions } from './WreathFormOptions'
 import axios from 'axios'
 
 function WreathForm() {
-    const [enteredSize, setEnteredSize] = useState(15)
-    const [enteredColor, setEnteredcolor] = useState([])
-    const [enteredSeason, setEnteredseason] = useState('Season')
-    const [enteredPrice, setEnteredprice] = useState('Price')
+    const [enteredMinSize, setEnteredMinSize] = useState(14)
+    const minSizeChangeHandler = (event) => setEnteredMinSize(event.target.value)
 
+    const [enteredMaxSize, setEnteredMaxSize] = useState(20)
+    const maxSizeChangeHandler = (event) => setEnteredMaxSize(event.target.value)
 
-    // USE THE FOLLOWING TO LOAD COLORS AND SEASON DROPDOWNS
-    // useEffect(() => {
-    //     fetch("/wreaths/").then(res => {
-    //         if (res.ok) {
-    //             return res.json()
-    //         }
-    //     }).then(jsonRes => setWreaths(jsonRes.wreathsList))
-    // })
+    const [enteredMinPrice, setEnteredMinPrice] = useState(15)
+    const minPriceChangeHandler = (event) => setEnteredMinPrice(event.target.value)
 
-    const colourOptions = [
-        { value:'red', label: 'Red' }, 
-        { value:'green', label: 'Green' }, 
-        { value:'blue', label: 'Blue' }, 
-    ]
+    const [enteredMaxPrice, setEnteredMaxPrice] = useState(45)
+    const maxPriceChangeHandler = (event) => setEnteredMaxPrice(event.target.value)
+
+    const [enteredColors, setEnteredcolors] = useState([])
+    const colorChangeHandler = (selectedColors) => {
+        setEnteredcolors(selectedColors)
+    };
+
+    const [enteredSeasons, setEnteredSeasons] = useState([])
+    const seasonChangeHandler = (selectedSeasons) => {
+        setEnteredSeasons(selectedSeasons)
+    };
+
+    const [enteredHolidays, setEnteredHolidays] = useState([])
+    const holidayChangeHandler = (selectedHolidays) => {
+        setEnteredHolidays(selectedHolidays)
+    };
+
+    const [wreaths, setWreaths] = useState([])
 
     const animatedComponents = makeAnimated()
 
-    const sizeChangeHandler = (event) => {
-        setEnteredSize(event.target.value)
-    }
-
-    const colorChangeHandler = (event) => {
-        setEnteredcolor(event.target.value)
-    }
-
-    const seasonChangeHandler = (event) => {
-        setEnteredseason(event.target.value)
-    }
-
-    const priceChangeHandler = (event) => {
-        setEnteredprice(event.target.value)
-    }
-
     const submitHandler = (event) => {
         event.preventDefault()
-        console.log(input)
+        const colorValues = []
+        for (let color_index in enteredColors) {
+            colorValues.push(enteredColors[color_index].value)
+        }
+
+        const seasonValues = []
+        for (let season_index in enteredSeasons) {
+            seasonValues.push(enteredSeasons[season_index].value)
+        }
+
+        const holidayValues = []
+        for (let holiday_index in enteredHolidays) {
+            holidayValues.push(enteredHolidays[holiday_index].value)
+        }
+
+        const searchCriteria = {
+            "minPrice": Number(enteredMinPrice),
+            "maxPrice": Number(enteredMaxPrice),
+            "minSize": Number(enteredMinSize),
+            "maxSize": Number(enteredMaxSize),
+            "colors": colorValues,
+            "timesOfTheYear": seasonValues.concat(holidayValues)
+        }
+
+        console.log(searchCriteria)
+        axios.post('http://localhost:3001/api/filterWreaths/', searchCriteria)
+            .then(res => {
+                const wreaths = res.data
+                setWreaths(wreaths)
+                console.log(wreaths)
+            })
+        
+
     }
 
     return (
+        <div>
         <form onSubmit={submitHandler} action=''>
             <div className='new-wreath__controls'>
                 <div className='new-wreath__control'>
-                    <label htmlFor=''>{enteredSize}</label> 
-                    <input default='18' type='number' min='14' step='1' onChange={sizeChangeHandler} /> 
+                    <label htmlFor=''>Enter min size</label>
+                    <input default={enteredMinSize} type='number' min='14' max={enteredMaxSize} step='1' onChange={minSizeChangeHandler} />
                 </div>
-                {/* <div className='new-wreath__control'>
-                    <label htmlFor=''>{enteredColor}</label> 
-                    <input type='text' onChange={colorChangeHandler} /> 
-                </div> */}
-                <Select className='colour_select'
+                <div className='new-wreath__control'>
+                    <label htmlFor=''>Enter max size</label>
+                    <input default={enteredMaxSize} type='number' min={enteredMinSize} max='24' step='1' onChange={maxSizeChangeHandler} />
+                </div>
+                <div className='new-wreath__control'>
+                    <label htmlFor=''>Enter min price</label>
+                    <input default={enteredMinPrice} type='number' min='15' max={enteredMaxPrice} step='1' onChange={minPriceChangeHandler} />
+                </div>
+                <div className='new-wreath__control'>
+                    <label htmlFor=''>Enter max price</label>
+                    <input default={enteredMaxPrice} type='number' min={enteredMinPrice} max='45' step='1' onChange={maxPriceChangeHandler} />
+                </div>
+                <div><h4>Select Color(s)</h4></div>
+                <Select className='multi_option_select'
                     closeMenuOnSelect={false}
                     components={animatedComponents}
                     isMulti
+                    value={enteredColors}
+                    onChange={colorChangeHandler}
                     options={colourOptions}
                 />
-                <div className='new-wreath__control'>
-                    <label htmlFor=''>{enteredSeason}</label> 
-                    <input type='text' onChange={seasonChangeHandler} /> 
-                </div>
-                <div className='new-wreath__control'>
-                    <label htmlFor=''>{enteredPrice}</label> 
-                    <input type='text' onChange={priceChangeHandler} /> 
-                </div>
+                <div><h4>Select Season(s)</h4></div>
+                <Select className='multi_option_select'
+                    closeMenuOnSelect={false}
+                    components={animatedComponents}
+                    isMulti
+                    value={enteredSeasons}
+                    onChange={seasonChangeHandler}
+                    options={seasonOptions}
+                />
+                <div><h4>Select Holiday(s)</h4></div>
+                <Select className='multi_option_select'
+                    closeMenuOnSelect={false}
+                    components={animatedComponents}
+                    isMulti
+                    value={enteredHolidays}
+                    onChange={holidayChangeHandler}
+                    options={holidayOptions}
+                />
             </div>
             <div className='new-wreath__actions'>
-                <button type='submit'>Add Wreath</button>
+                <button type='submit'>Search Wreaths</button>
             </div>
         </form>
+        <div>
+        {
+            wreaths
+            .map(wreath =>
+                <div>
+                {wreath.title}
+                <img src={wreath.pictureurl[0]} alt={wreath.title} width="150" height="200"/>
+                </div>
+            )
+        }
+        </div>
+        </div>
     )
 }
 
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { config } from 'dotenv'
-config()
-import { init } from './server'
-init()
